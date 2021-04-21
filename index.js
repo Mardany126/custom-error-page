d79c1af444abc94afe9bbfe58334eb05b0bfc471
Max Mardany <Max@Maxs-MacBook-Air.local>
2021-04-20 20:55:31 -0400

    added app.use added app.all added app.listen into my index.js file

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express')
-const app = express
 
+const app = express()
+
+app.use(express.static('public'))
+
+app.all('*', (request, response) => {
+    return response.status(404).send('')
+})
+
+app.listen(1337)
